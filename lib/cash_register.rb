7ba4f1a97f8cfc274ff4d4ef95e33b9e5455f6f9
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-24 19:35:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+class CashRegister
+  attr_accessor :total,:discount
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount / 100.0
+    #@items = []
+    
+  end 
+  
+  def add_item(title,price,quantity = 0)
+    @title = title
+    @price = price
+    self.total += @price if quantity == 0 
+    self.total += @price * quantity unless quantity == 0 
+  end
+  
+  def apply_discount
+  
+  end 
+end
