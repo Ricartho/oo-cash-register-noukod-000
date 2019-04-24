2871c786400be364b457a36b265fca5b48a8e553
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-24 19:55:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,11 +16,11 @@ class CashRegister
   
   def apply_discount
     if @discount == 0 
-      puts "There is no discount to apply."
+       "There is no discount to apply."
     else
       @total -= (@total*(@discount / 100.0)).to_i
       
-      puts"After the discount, the total comes to $#{@total}."
+      "After the discount, the total comes to $#{@total}."
       
     end 
   end 
